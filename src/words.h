a9a13d5c0b1ef08c4db5a9813539c4ebe20c2eac
2ziu <piotr.tuz@gmail.com>
2017-10-08 23:02:15 +0200

    add

--- src/words.h
+++ src/words.h
@@ -13,6 +13,7 @@ class Words{
     CharState tab[16];
 
     void build(Trie& trie, Positon pos);
+    void init();
 
     std::vector<Positon> getPositons(Positon pos) const;
     bool validate(Positon pos) const{
